16d1babc93765a7be0054cb8f2bf2934f072edd6
Shannon Alexander <shannonmariealexander94@gmail.com>
2021-01-11 19:21:48 -0500

    created and organzied variables in script to be used as characters

---
+++ script.js
@@ -0,0 +1,12 @@
+// Declare a variable and assign it a value
+var generateBtn = document.querySelector("#generate");
+var passwordCharacters = [];
+var uppercaseLettersArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseLettersArray = ["a", "b", "c",	"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharactersArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@"];
+var passwordLength;
+var uppercaseLetters;
+var lowercaseLetters;
+var numbers;
+var specialCharacters;
